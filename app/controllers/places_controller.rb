2fe95a641b17e629bff9d66e2cc6bb38049e0fe4
hereismyusername <my.e.address123@gmail.com>
2017-07-21 16:27:41 +0000

    destroy place

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -20,8 +20,20 @@ class PlacesController < ApplicationController
     @place = Place.find(params[:id])
   end
 
-  def
+  def edit
     @place = Place.find(params[:id])
+  end
+
+  def update
+    @place = Place.find(params[:id])
+    @place.update_attributes(place_params)
+    redirect_to root_path
+  end
+
+  def destroy
+    @place = Place.find(params[:id])
+    @place.destroy
+    redirect_to root_path
   end
 
   private
