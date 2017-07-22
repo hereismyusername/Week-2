3a7a63827b65b2b2cc66e6e26ca39c6ece9332f5
hereismyusername <my.e.address123@gmail.com>
2017-07-22 13:21:24 +0000

    add back pagination

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,8 +3,8 @@ class PlacesController < ApplicationController
 
   def index
     @places = Place.paginate(:page => params[:page])
-    #@places = Place.all.paginate(page: params[:page], per_page: 3)
-    #@places = Place.paginate(:page => params[:page], :per_page => 3)
+    @places = Place.all.paginate(page: params[:page], per_page: 3)
+    @places = Place.paginate(:page => params[:page], :per_page => 3)
   end
 
   def new
