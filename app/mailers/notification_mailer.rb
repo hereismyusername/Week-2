ffa4c21005ea21ca0abca180e4093e65b215aeb4
hereismyusername <my.e.address123@gmail.com>
2017-08-01 11:17:11 +0000

    preview auto email

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ApplicationMailer
   default from: "no-reply@nomsterapp.com"
 
   def comment_added
-    mail(to: "user.contact.email.address@gmail.com",
+    mail(to: "INSERT EMAIL ADDRESS",
           subject: "A comment has been added to your place")
   end
 end
